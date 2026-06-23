2e4b6a191c59b1cb1ce7e74cb9d449a540bc43f0
rafael rocha hernandez <rafaelrh.dev@gmail.com>
2026-06-23 12:51:39 -0600

    arreglo horarios y diplomados

--- assets/js/script.js
+++ assets/js/script.js
@@ -41,7 +41,7 @@ const legendHours = document.getElementById('legendHours');
 const bookingForm = document.getElementById('bookingForm');
 const formMsg = document.getElementById('formMsg');
 
-let HORARIOS = ['15:00', '16:00', '17:00', '18:00', '19:00', '20:00'];
+let HORARIOS = ['10:00','11:00','12:00','13:00','14:00','15:00', '16:00', '17:00', '18:00', '19:00', '20:00', ];
 let OCUPADAS = []; // [{fecha, hora, estado}]
 
 // Fecha mínima = hoy
